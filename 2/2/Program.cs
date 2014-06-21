1271ab731938ec2d60feee0cb519974c3e204b44
Bilal Durrani <bdurrani-github@outlook.com>
2014-06-21 00:06:37 -0400

    framework for 2nd question

---
+++ 2/2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            List<string> setList = new List<string>();
+            for (int x = 0; x < num; x++)
+            {
+                setList.Add(Console.ReadLine());
+            }
+            foreach (string item in setList)
+            {
+                string[] stringset = item.Split(new[] {' '});
+                int min = int.Parse(stringset[0]);
+                int max = int.Parse(stringset[1]);
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
