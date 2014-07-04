e5db1a5b418b98df974308a04d51ea8b4d07d9d4
Bilal Durrani <bilal33@gmail.com>
2014-07-04 15:55:36 -0400

    combination sieve with basic checking.
    tested using range 100-200

--- 2/2/Program.cs
+++ 2/2/Program.cs
@@ -7,6 +7,11 @@ namespace _2
     {
         static void Main(string[] args)
         {
+            primes.Add(2, 2);
+            primes.Add(3, 3);
+            primes.Add(5, 5);
+            PrintPrimes(100, 200);
+            Console.ReadLine();
             int num = int.Parse(Console.ReadLine());
             List<string> setList = new List<string>();
             for (int x = 0; x < num; x++)
@@ -15,11 +20,80 @@ namespace _2
             }
             foreach (string item in setList)
             {
-                string[] stringset = item.Split(new[] {' '});
+                string[] stringset = item.Split(new[] { ' ' });
                 int min = int.Parse(stringset[0]);
                 int max = int.Parse(stringset[1]);
-                Console.WriteLine(item);
             }
+
+        }
+        private static SortedList<int, int> primes = new SortedList<int, int>();
+        public static int MaxPrimeFound = -1;
+        public static void PrintPrimes(int min, int max)
+        {
+            bool[] isNotPrimeArray = new bool[max - min + 1];
+            for (int check = min; check <= max; check++)
+            {
+                if (check == 1)
+                {
+                    isNotPrimeArray[check - min] = true;
+                    continue;
+                }
+                int index = 3;
+                bool isPrime = true;
+                if (!primes.ContainsKey(check))
+                {
+                    if (isNotPrimeArray[check - min])
+                    {
+                        continue;
+                    }
+                    if (check % 2 == 0)
+                    {
+                        isPrime = false;
+                    }
+                    else
+                    {
+                        for (int i = index; i <= Math.Floor(Math.Sqrt(check)); i += 2)
+                        {
+                            if (check % i == 0)
+                            {
+                                isPrime = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (isPrime)
+                {
+                    if (!primes.ContainsKey(check))
+                    {
+                        primes.Add(check, check);
+                    }
+                    if (check > MaxPrimeFound)
+                    {
+                        MaxPrimeFound = check;
+                    }
+                }
+                else
+                {
+                    isNotPrimeArray[check - min] = true;
+                }
+                for (int i = 2 * check; i <= max; i += check)
+                {
+                    isNotPrimeArray[i - min] = true;
+                }
+            }
+            int counter = 0;
+            int primecounter = 0;
+            foreach (bool item in isNotPrimeArray)
+            {
+                if (!item)
+                {
+                    Console.WriteLine(min + counter);
+                    primecounter++;
+                }
+                counter++;
+            }
+            primecounter = 0;
         }
     }
 }
